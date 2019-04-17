6149b3176adacc06b8bcf185012c3122f672b44d
Yugal Tale <yugaltale5@gmail.com>
2019-04-17 20:56:19 +0530

    Run this file on Windows OS.
    Run this file as pyhton tsa.py

---
+++ tsa.py
@@ -0,0 +1,13 @@
+import os
+print('#######################################################################################################')
+os.system('figlet Twitter Sentiment Analyisis')
+print('Note:1)Polarity=in sentiment analysis it refers to identifying sentiment orientation (positive, neutral, and negative).')
+print('2)Subjectivity= expressions are opinions that describe people’s feelings towards a specific subject or topic.')
+print('3)Assisment= Analysis of special word in the tweet.')
+print('#######################################################################################################')
+os.system('python ui.py > o.txt')
+os.system('python test.py > op.txt')
+#os.system('kate op.txt')
+file=open("op.txt","r")
+opt=file.read()
+os.system('notepad op.txt')
